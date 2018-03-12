9936bad31341d50cd57461e2709f11fa5b569d8b
JanaMorgan <35313373+JanaMorgan@users.noreply.github.com>
2018-03-12 15:48:42 -0500

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+//my js file
+
+$(document).ready(function() {
+  var indexItems = document.getElementsByTagName("*");
+  for(var i=0, max=indexItems.length; i < max; i++){
+  console.log(indexItems[i].tagName);
+  }
+});
